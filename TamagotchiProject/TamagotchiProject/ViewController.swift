da9da8d9c64d958c0288214d946d003099a9fede
Yujin Lee <yj587878@gmail.com>
2023-08-04 15:26:12 +0900

    [ADD] 프로젝트 생성

---
+++ TamagotchiProject/TamagotchiProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TamagotchiProject
+//
+//  Created by 이유진 on 2023/08/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
