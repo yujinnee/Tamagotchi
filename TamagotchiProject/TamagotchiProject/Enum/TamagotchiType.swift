54a2995897e18a220b14c4fb0c00b1c39ae65d70
Yujin Lee <yj587878@gmail.com>
2023-08-05 16:36:51 +0900

    [FEAT] 다마고치 타입 열거형 구현

--- TamagotchiProject/TamagotchiProject/Enum/TamagotchiType.swift
+++ TamagotchiProject/TamagotchiProject/Enum/TamagotchiType.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+enum TamagotchiType{
+    case Takkeum
+    case Bangsil
+    case Banzzack
+    
+    var title: String{
+        switch self{
+        case .Bangsil: return "방실방실다마고치"
+        case .Banzzack: return "반짝반짝다마고치"
+        case .Takkeum: return "따끔따끔다마고치"
+        }
+    }
+    var imageNum: Int{
+        switch self{
+        case .Takkeum: return 1
+        case .Bangsil: return 2
+        case .Banzzack: return 3
+        }
+    }
+    
+}
