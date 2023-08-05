46d49d678a584407f0ca01190096caf3dd1f19ea
Yujin Lee <yj587878@gmail.com>
2023-08-05 14:22:49 +0900

    [ADD] 컬러,아이콘 에셋 추가

---
+++ TamagotchiProject/TamagotchiProject/Constants/Color.swift
@@ -0,0 +1,20 @@
+//
+//  Color.swift
+//  TamagotchiProject
+//
+//  Created by 이유진 on 2023/08/05.
+//
+
+import UIKit
+
+enum TMUIColor{
+    static let backgroundColor = TMColorPallete.lightBlue
+    static let fontColor = TMColorPallete.heavyBlue
+    static let borderColor = TMColorPallete.heavyBlue
+    
+}
+
+enum TMColorPallete {
+    static let lightBlue = UIColor(red: 245/255, green: 252/255, blue: 252/255, alpha: 1)
+    static let heavyBlue = UIColor(red: 77/255, green: 106/255, blue: 120/255, alpha: 1)
+}
---
+++ TamagotchiProject/TamagotchiProject/Constants/Icon.swift
@@ -0,0 +1,18 @@
+//
+//  Icon.swift
+//  TamagotchiProject
+//
+//  Created by 이유진 on 2023/08/05.
+//
+
+import UIKit
+
+enum Icon{
+    static let feed = UIImage(systemName: "drop.circle")
+    static let water = UIImage(systemName: "leaf.circle")
+    static let pencil = UIImage(systemName: "pencil")
+    static let moon  = UIImage(systemName: "moon.fill")
+    static let refresh = UIImage(systemName: "arrow.clockwise")
+}
+
+
