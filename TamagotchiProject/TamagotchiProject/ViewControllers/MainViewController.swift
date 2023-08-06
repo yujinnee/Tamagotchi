696caed50c8d2e44b1012cc5a9723814b9b76519
Yujin Lee <yj587878@gmail.com>
2023-08-07 01:50:35 +0900

    [FEAT] 키보드 관련 함수 구현

--- TamagotchiProject/TamagotchiProject/ViewControllers/MainViewController.swift
+++ TamagotchiProject/TamagotchiProject/ViewControllers/MainViewController.swift
@@ -34,6 +34,7 @@ class MainViewController: UIViewController,InitialSetting {
         super.viewDidLoad()
         initUI()
         initData()
+        setKeyboardObserver()
 
     
     }
@@ -88,4 +89,40 @@ class MainViewController: UIViewController,InitialSetting {
         descriptionLabel.text = info ?? "다마고치 정보"
         
     }
+    @IBAction func viewDidTap(_ sender: Any) {
+        view.endEditing(true)
+    }
 }
+
+extension MainViewController {
+    
+    func setKeyboardObserver() {
+        NotificationCenter.default.addObserver(self, selector: #selector(MainViewController.keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(MainViewController.keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object:nil)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+          if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+                  let keyboardRectangle = keyboardFrame.cgRectValue
+                  let keyboardHeight = keyboardRectangle.height
+              UIView.animate(withDuration: 1) {
+                  self.view.window?.frame.origin.y -= keyboardHeight
+              }
+          }
+      }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.window?.frame.origin.y != 0 {
+            if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+                    let keyboardRectangle = keyboardFrame.cgRectValue
+                    let keyboardHeight = keyboardRectangle.height
+                UIView.animate(withDuration: 1) {
+                    self.view.window?.frame.origin.y += keyboardHeight
+                }
+            }
+        }
+    }
+}
+
+
