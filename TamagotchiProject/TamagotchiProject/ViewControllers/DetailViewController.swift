d5c19817dc5b63f36629946f80ff8d0c404a55b1
Yujin Lee <yj587878@gmail.com>
2023-08-06 22:29:26 +0900

    [FEAT] 상세화면 레이아웃 구현

--- TamagotchiProject/TamagotchiProject/ViewControllers/DetailViewController.swift
+++ TamagotchiProject/TamagotchiProject/ViewControllers/DetailViewController.swift
@@ -7,24 +7,59 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController,InitialSetting {
     static let identifier = "DetailViewController"
 
+    @IBOutlet var alertBackgroundView: UIView!
+    @IBOutlet var thumbnailImageView: UIImageView!
+    @IBOutlet var borderLineView: UIView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var lineView: UIView!
+    @IBOutlet var introductionLabel: UILabel!
+    @IBOutlet var underLineView: UIView!
+    @IBOutlet var cancelButton: UIButton!
+    @IBOutlet var startButton: UIButton!
+    
+    var name: String?
+    var introduction: String?
+    var imageName: String?
     override func viewDidLoad() {
         super.viewDidLoad()
+        initUI()
+        initData()
 
-        // Do any additional setup after loading the view.
+    }
+    func initUI() {
+        alertBackgroundView.backgroundColor = TMUIColor.backgroundColor
+        
+        borderLineView.layer.borderColor = TMUIColor.borderColor.cgColor
+        borderLineView.layer.borderWidth = 1
+        borderLineView.layer.cornerRadius = 3
+        borderLineView.backgroundColor = TMUIColor.backgroundColor
+        
+        titleLabel.font = UIFont.systemFont(ofSize: 10, weight: .regular)
+        titleLabel.textColor = TMUIColor.fontColor
+        
+        lineView.backgroundColor = TMUIColor.borderColor
+        
+        introductionLabel.font = UIFont.systemFont(ofSize: 10, weight: .semibold)
+        
+        underLineView.backgroundColor = .lightGray
+        
+        cancelButton.setTitle("취소", for: .normal)
+        cancelButton.backgroundColor = .systemGray5
+        cancelButton.tintColor = TMUIColor.fontColor
+        
+        startButton.setTitle("시작하기", for: .normal)
+        startButton.tintColor = TMUIColor.fontColor
+    }
+    
+    func initData() {
+        titleLabel.text = name ?? "이름"
+        introductionLabel.text = introduction ?? "소개"
+        thumbnailImageView.image = UIImage(named: imageName ?? "noImage" ) ?? UIImage()
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
